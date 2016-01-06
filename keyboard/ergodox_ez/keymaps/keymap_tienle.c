31c637c6309dacb34e706d3f34e33fdf55b7f819
Tien Le <tienlx@gmail.com>
2016-01-06 15:15:33 +0700

    Avoid L1 from ~

--- keyboard/ergodox_ez/keymaps/keymap_tienle.c
+++ keyboard/ergodox_ez/keymaps/keymap_tienle.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      KC_FN1,  LCTL(KC_A),KC_ESC,
+        KC_GRV,KC_QUOT,      KC_FN1,  LCTL(KC_A),KC_ESC,
                                               ALT_T(KC_APP),  KC_HOME,
                                                               KC_END,
                                                KC_ENT,KC_SPC,KC_LGUI,
